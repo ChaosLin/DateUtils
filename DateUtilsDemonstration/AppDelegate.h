b018a5cb1b0b3fe6cda9e91f4810d75dcabafba7
Renton Lin <linweichao12@163.com>
2014-04-13 15:33:19 +0800

    Initial Commit

---
+++ DateUtilsDemonstration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DateUtilsDemonstration
+//
+//  Created by Chaos Lin on 4/13/14.
+//  Copyright (c) 2014 Chaos Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
